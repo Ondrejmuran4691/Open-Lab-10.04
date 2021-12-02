67c563f9b9e525ed1df99b771a6bd46c61732082
Ondrejmuran4691 <onko.muran@gmail.com>
2021-12-02 16:24:12 +0100

    Add project files.

---
+++ Open-Lab-10.04/Book.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._04
+{
+    public class Book
+    {
+        public string Title;
+
+        private int pages;
+        public int Pages
+        {
+
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 1;
+                }
+                pages = value;
+            }
+
+        }
+
+        public string category;
+        public string author;
+        
+        private int releaseDate;
+        public int ReleaseDate
+        {
+
+            get
+            {
+                return releaseDate;
+            }
+            set
+            {
+                if (value < 1405)
+                {
+                    value = -1;
+                }
+                else
+                {
+                    if (value > 2021)
+                    {
+                        value = -1;
+                    }
+                }
+                releaseDate = value;
+            }
+
+        }
+
+
+        public void writeall()
+        {
+
+            Console.WriteLine(Title);
+            Console.WriteLine(Pages);
+            Console.WriteLine(category);
+            Console.WriteLine(author);
+            Console.WriteLine(ReleaseDate);
+        }
+    }
+}
---
+++ Open-Lab-10.04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._04
+{ 
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Book LOTR = new Book();
+        LOTR.Title = "LOTR";
+        LOTR.author = "J. R. R. Tolkien";
+        LOTR.category = "Fantasy";
+        LOTR.Pages = -9250;
+        LOTR.ReleaseDate = -1954;
+
+
+            LOTR.writeall();
+
+    }
+}
+}
