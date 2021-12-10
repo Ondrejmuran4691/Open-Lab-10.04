4330f0197258a2b12c7d86ac4e9069bc35e37b1e
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-10 14:08:31 +0100

    create book with possibility of using more constructers

---
+++ Open-Lab-10.04/Open-Lab-10.04/Book.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._04
+{
+    public class Book
+    {
+        public Book()
+        {
+            Title = "-1";
+            ReleaseDate = -1;
+            Author = "-1";
+            Pages = -1;
+            Category = "-1";
+        }
+
+        public Book(string titleofbook, int pagesofbook)
+        {
+            Title = titleofbook;
+            ReleaseDate = -1;
+            Author = "-1";
+            Pages = pagesofbook;
+            Category = "-1";
+        }
+        public Book(string titleofbook, string authorofbook, string categoryofbook, int pagesofbook, int releasedateofbook)
+        {
+            Title = titleofbook;
+            ReleaseDate = releasedateofbook;
+            Author = authorofbook;
+            Pages = pagesofbook;
+            Category = categoryofbook;
+        }
+        public string Title { set; get; }
+
+        private int pages;
+        public int Pages
+        {
+
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 1;
+                }
+                pages = value;
+            }
+
+        }
+
+        public string Category { set; get; }
+        public string Author { set; get; }
+
+        private int releaseDate;
+        public int ReleaseDate
+        {
+
+            get
+            {
+                return releaseDate;
+            }
+            set
+            {
+                if (value < 1405)
+                {
+                    value = -1;
+                }
+                else
+                {
+                    if (value > 2021)
+                    {
+                        value = -1;
+                    }
+                }
+                releaseDate = value;
+            }
+
+        }
+
+
+        
+        public void writeall()
+        {
+
+            Console.WriteLine(Title);
+            Console.WriteLine(Pages);
+            Console.WriteLine(Category);
+            Console.WriteLine(Author);
+            Console.WriteLine(ReleaseDate);
+        }
+
+        
+            
+
+
+    }
+}
---
+++ Open-Lab-10.04/Open-Lab-10.04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._04
+{ 
+class Program
+{
+    static void Main(string[] args)
+    {
+
+            Book LOTR = new Book();
+           
+
+        LOTR.Title = "LOTR";
+        LOTR.Author = "J. R. R. Tolkien";
+        LOTR.Category = "Fantasy";
+        LOTR.Pages = 9250;
+        LOTR.ReleaseDate = 1954;
+
+            Book LOTR2 = new Book();
+            Book LOTR3 = new Book("LOTR3", 156);
+            Book HOBIT = new Book("HOBIT", "J. R. R. Tolkien", "Fantasy", 6544, 5);
+            LOTR.writeall();
+            LOTR2.writeall();
+            LOTR3.writeall();
+            HOBIT.writeall();
+        }
+}
+}
